2be9c08486bd53456ff71a32791337a6b26890a2
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-06-30 17:02:47 +0200

    Added chicken and randomized the generation.

--- models/world.class.js
+++ models/world.class.js
@@ -1,12 +1,35 @@
 class World {
     character = new Character();
     enemies = [
-        new Chicken,
-        new Chicken,
-        new Chicken,
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
     ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
+        this.draw();
+    }
 
     draw() {
 
+        //deletes the img character of pepe
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        //generates the img character of pepe
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+        //generates chicken img of enemies chicken
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+        })
+
+        //draw will call multiple times, depends on gpu
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
