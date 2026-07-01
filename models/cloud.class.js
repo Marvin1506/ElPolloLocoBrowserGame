3b664bcdbf57719edbc922cda02b369bd914848d
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-01 14:49:09 +0200

    Added Background Objects into object Array.
    Also changed the height and width attributes of the cloudes. Our parameters are now right.

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,10 @@
 class Cloud extends movableObject{
     y = 0;
-    height = 600;
-    width = 300
+    height = 400;
+    width = 720;
     constructor() {
         super().loadImage("img/5_background/layers/4_clouds/1.png");
 
-        this.x = Math.random() * 500;
+        this.x = Math.random() * 20;
     }
 }
