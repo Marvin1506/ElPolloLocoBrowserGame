6c19847b35fe7e7ab9c76ff454dbb2cbe1a3032e
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-02 00:17:49 +0200

    Added bakcground and sizing of character and enemies.

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,19 @@
 class Cloud extends movableObject{
     y = 0;
-    height = 400;
+    height = 300;
     width = 720;
     constructor() {
         super().loadImage("img/5_background/layers/4_clouds/1.png");
 
-        this.x = Math.random() * 20;
+        this.x = Math.random() * 100;
+        this.animate();
+
     }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+    }
+    
 }
