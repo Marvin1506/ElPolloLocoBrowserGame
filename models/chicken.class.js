9a2d725a02b6f61711bbc958c0da485896aaee7b
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-02 13:13:15 +0200

    Same function for walking pepe copied for the enemy chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,9 +2,27 @@ class Chicken extends movableObject{
     y = 360;
     height = 60;
     width = 80;
+    IMAGES_WALKING = [
+        "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+    ];
+    currentImage = 0;
+
     constructor() {
         super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
         this.x = 200 + Math.random() * 500;
+    }
+
+    animate() {
+
+        setInterval( () => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 }
