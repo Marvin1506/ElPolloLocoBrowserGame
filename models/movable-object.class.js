f2b224fa63f80aa1229c5f52b383ed967070fab7
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-03 14:20:07 +0200

    Added endboss into html and added the class object. Play animation outsourced.
    Play animation is now outsourced.
    Every object will show the picture array with the playAnimation function that is in the extend class movabale-object.class.js

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,16 +4,18 @@ class movableObject {
     img;
     height = 150;
     width = 100;
-    imageCache = {};
+    imageCache = {}; //savepoint of images that are already loaded
     currentImage = 0;
     speed = 0.15;
-    otherDirection = false;
+    otherDirection = false; // 
 
+    //loads the image of the object and sets the src of the image to the path of the image
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    //turns the objects into imgs and pushes the already loaded imgs into the imageCache object array
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
@@ -22,10 +24,20 @@ class movableObject {
         });
     }
 
+    //plays the animation of the object by changing the image of the object
+    playAnimation(images) {
+        let i = this.currentImage % images.length; // for loop that resets and begins again when the last image is reached
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+    //moves the object to the right by changing the x position of the object.
     moveRight() {
         console.log("Moving right");
     }
 
+    //moves the object to the left by changing the x position of the object.
     moveLeft() {
         setInterval(() => {
             this.x -= this.speed;
