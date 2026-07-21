67094bb4cd869ce4d1d2a9ae9c6526984600cb50
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-21 20:44:12 +0200

    Adding a random salsa bottle with the images bottles array and the math.floor and math random code line.

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,11 +1,19 @@
 class Bottle extends DrawableObject {
+
+    IMAGES_BOTTLES = [
+        "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
+        "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
+    ];
+
     width = 80;
     height = 80;
     x = 150;
     y = 350;
 
     constructor() {
-        super().loadImage("img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+        super();
+        const randomImage = this.IMAGES_BOTTLES[Math.floor(Math.random() * this.IMAGES_BOTTLES.length)];
+        this.loadImage(randomImage);
         this.x = 200 + Math.random() * 500;
         this.y = 350;
     }
