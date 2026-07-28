bebbe80934ba9d4ff70ec38b8ee5cc2bb0ec9db0
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-07-28 02:14:48 +0200

    Update drawable-object.class.js

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -33,8 +33,8 @@ class DrawableObject {
         });
     }
 
-    drawFrame(ctx) {
-        if(this instanceof Endboss){
+    /*drawFrame(ctx) {
+        if(this instanceof Character || this instanceof ThrowableObject || this instanceof Endboss){
             ctx.beginPath();
             ctx.lineWidth = "2";
             ctx.strokeStyle = "blue";
@@ -52,5 +52,5 @@ class DrawableObject {
             );
             ctx.stroke();
         }
-    }
+    }*/
 }
